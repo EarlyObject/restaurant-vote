b58405b9def9c3e5fec8e869188b87295811bc7d
EarlyObject <sr.ardak@gmail.com>
2020-03-31 17:47:52 +0200

    JSON support added

--- src/main/java/com/sar/ws/ui/controller/UserController.java
+++ src/main/java/com/sar/ws/ui/controller/UserController.java
@@ -1,12 +1,18 @@
 package com.sar.ws.ui.controller;
 
+import com.sar.ws.exceptions.UserServiceException;
 import com.sar.ws.service.UserService;
 import com.sar.ws.shared.dto.UserDto;
 import com.sar.ws.ui.model.request.UserDetailsRequestModel;
+import com.sar.ws.ui.model.response.ErrorMessages;
 import com.sar.ws.ui.model.response.UserRest;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import javax.jws.soap.SOAPBinding;
+import java.awt.*;
 
 @RestController
 @RequestMapping("users")//http://localhost:8080/users
@@ -15,7 +21,7 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @GetMapping(path={"/{id}"})
+    @GetMapping(path={"/{id}"}, produces = {MediaType.APPLICATION_JSON_VALUE})
     public UserRest getUser(@PathVariable String id) {
         UserRest returnValue = new UserRest();
 
@@ -25,9 +31,15 @@ public class UserController {
         return returnValue;
     }
 
-    @PostMapping
-    public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails){
+    @PostMapping(
+            consumes = {MediaType.APPLICATION_JSON_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE})
+    public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) throws UserServiceException {
       UserRest returnValue = new UserRest();
+
+      if (userDetails.getFirstName().isEmpty()) {
+          throw new UserServiceException(ErrorMessages.MISSING_REQUIRED_FIELD.getErrorMessage());
+      }
 
         UserDto userDto = new UserDto();
         BeanUtils.copyProperties(userDetails, userDto);
